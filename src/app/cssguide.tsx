e4d73ea03e2bf40896a8995caf32f7b9750d0fae
unknown <fomexii@hotmail.com>
2025-06-05 16:07:46 +0700

    .

--- src/app/cssguide.tsx
+++ src/app/cssguide.tsx
@@ -1,4 +1,8 @@
-export default function cssgGuide ({children}: {children: React.ReactNode}) {
-  return (
- '.')
+export default function CssgGuide({ children }: { children: React.ReactNode }) {
+    return (
+        <><div className="flex-1 w-full flex-col">
+  <div className="card bg-base-300 rounded-box grid h-full place-items-center">content</div></div>
+            <div> {children}</div>
+        </>
+    )
 };
