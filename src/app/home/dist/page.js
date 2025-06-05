6dc6a40aa6ba4916e5ebac67c7dfc1fdf305e6f7
unknown <narabadee.sirivitoon@g.swu.ac.th>
2025-06-05 15:10:20 +0700

    add all files

---
+++ src/app/home/dist/page.js
@@ -0,0 +1,9 @@
+"use client";
+"use strict";
+exports.__esModule = true;
+function HomePage() {
+    return (React.createElement("div", { style: { maxWidth: 600, margin: "100px auto", padding: 32, border: "1px solid #ccc", borderRadius: 12, textAlign: "center" } },
+        React.createElement("h1", null, "\u0E22\u0E34\u0E19\u0E14\u0E35\u0E15\u0E49\u0E2D\u0E19\u0E23\u0E31\u0E1A!"),
+        React.createElement("p", null, "\u0E04\u0E38\u0E13\u0E40\u0E02\u0E49\u0E32\u0E2A\u0E39\u0E48\u0E23\u0E30\u0E1A\u0E1A\u0E2A\u0E33\u0E40\u0E23\u0E47\u0E08\u0E41\u0E25\u0E49\u0E27")));
+}
+exports["default"] = HomePage;
