ce3967d616aeb2ccc761923beb1f1cc4cae9b2d6
unknown <fomexii@hotmail.com>
2025-06-06 10:49:34 +0700

    q

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -5,9 +5,43 @@ import CssgGuide from '../cssguide'
 export default function HomePage() {
   return (
     <CssgGuide>
-    <div>
-        <h1>Assignment</h1>
-    </div>
+      <div className="overflow-x-auto flex justify-center py-8">
+        <div className="bg-base-100 rounded-xl shadow-xl border border-base-300 w-full max-w-5xl">
+          <table className="table table-zebra w-full">
+            {/* head */}
+            <thead>
+              <tr>
+                <th>
+                  <label>
+                  </label>
+                </th>
+                <th>Name</th>
+                <th>Job</th>
+                <th>Favorite Color</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* No data row */}
+              <tr>
+                <td colSpan={5} className="text-center text-gray-400 py-12">
+                  No data available.
+                </td>
+              </tr>
+            </tbody>
+            {/* foot */}
+            <tfoot>
+              <tr>
+                <th></th>
+                <th>Name</th>
+                <th>Job</th>
+                <th>Favorite Color</th>
+                <th></th>
+              </tr>
+            </tfoot>
+          </table>
+        </div>
+      </div>
     </CssgGuide>
   )
 }
